259e2b7df379f8aca6a89fda53cf689a8e5fe669
Ariel Estigarribia <arielestigarribia@gmail.com>
2017-10-29 13:31:39 -0300

    test

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 'use strict';
 
+const filters = [
+  { 'vendorId': 0x2338, 'productId': 0x27256 },
+  { 'vendorId': 0x2338, 'productId': 0x32772 },
+  { 'vendorId': 0x2338, 'productId': 0x32771 },
+  { 'vendorId': 0x2338, 'productId': 0x32777 },
+];
 
 function initScale() {
 
   
-  navigator.usb.requestDevice({ filters: [{ vendorId: 0x2338 }] })
+  navigator.usb.requestDevice({ 'filters': filters })
   .then(device => {
     console.log(device.productName);      // "Arduino Micro"
     console.log(device.manufacturerName); // "Arduino LLC"
@@ -22,5 +28,6 @@ function initScale() {
 }
 
 document.querySelector("#initScale").onclick = function() {
+  console.log('click');
   initScale();
 };
