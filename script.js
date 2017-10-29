8a2580489d322ae1a14f56313f43f1df3e7688fd
Ariel Estigarribia <arielestigarribia@gmail.com>
2017-10-29 13:14:41 -0300

    test

--- script.js
+++ script.js
@@ -2,12 +2,23 @@
 
 
 function initScale() {
+
+  
   navigator.usb.requestDevice({ filters: [{ vendorId: 0x2338 }] })
   .then(device => {
     console.log(device.productName);      // "Arduino Micro"
     console.log(device.manufacturerName); // "Arduino LLC"
   })
   .catch(error => { console.log(error); });
+  
+
+ //  navigator.usb.getDevices().then(devices => {
+	//   devices.map(device => {
+	//     console.log(device.productName);      // "Arduino Micro"
+	//     console.log(device.manufacturerName); // "Arduino LLC"
+	//   });
+	// })
+
 }
 
 document.querySelector("#initScale").onclick = function() {
