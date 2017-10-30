3ef45a733a9196be42e1c33e84baf9773fc444ca
Ariel Estigarribia <arielestigarribia@gmail.com>
2017-10-30 12:17:08 -0300

    cambios

--- script.js
+++ script.js
@@ -8,22 +8,21 @@ const filters = [
 ];
 
 function initScale() {
-
   
   navigator.usb.requestDevice({ 'filters': filters })
   .then(device => {
-    console.log(device.productName);      // "Arduino Micro"
-    console.log(device.manufacturerName); // "Arduino LLC"
+    console.log(device.productName);      
+    console.log(device.manufacturerName); 
   })
   .catch(error => { console.log(error); });
   
 
- //  navigator.usb.getDevices().then(devices => {
-	//   devices.map(device => {
-	//     console.log(device.productName);      // "Arduino Micro"
-	//     console.log(device.manufacturerName); // "Arduino LLC"
-	//   });
-	// })
+    navigator.usb.getDevices().then(devices => {
+	  devices.map(device => {
+	    console.log(device.productName);      
+	    console.log(device.manufacturerName); 
+	  });
+	})
 
 }
 
@@ -31,3 +30,47 @@ document.querySelector("#initScale").onclick = function() {
   console.log('click');
   initScale();
 };
+
+
+// let button = document.getElementById('initScale');
+// button.addEventListener('click', async () => {
+//   let device;
+//   console.log('click 1');
+//   try {
+//     console.log('click 2');
+//     device = await navigator.usb.requestDevice({ filters: [{
+//         vendorId: 0x2338,
+//         productId: 0x32771
+//     }]});
+//   } catch (error) {
+//     console.log(error); 
+//     console.log('click 3');
+//   }
+
+//   if (device !== undefined) {
+//     // Add |device| to the UI.
+//   }
+// });
+
+
+// document.addEventListener('DOMContentLoaded', async () => {
+//   console.log('debug 1');
+//   let devices = await navigator.usb.getDevices();
+//   devices.forEach(device => {
+//     console.log(device.productName);
+// 	   console.log(device.manufacturerName);
+// 	console.log('debug 2');
+//   });
+// });
+
+
+// navigator.usb.addEventListener('connect', device => {
+//   // Add |device| to the UI.
+//   console.log('connect');
+// });
+
+
+// navigator.usb.addEventListener('disconnect', device => {
+//   // Remove |device| from the UI.
+//   console.log('disconnect');
+// });
